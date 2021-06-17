d0708c6ef45f48caaaaa8a517aec66415221fbc4
Andrey Shinkevich <andrew.shinkevich@gmail.com>
2021-06-17 21:45:20 +0300

    vim key mappings: telescope with ctrl, hjkl -> ijkl, i -> h

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,16 +3,13 @@ require('telescope').load_extension('heading')
 local set_keymap = require('../common').set_keymap
 set_keymap(
     'n',
-    '<leader>ff',
+    '<c-f>',
     '<cmd>Telescope find_files find_command=fd,--hidden,--no-ignore,--exclude,*.git,--type,f<cr>'
 )
-set_keymap('n', '<leader>fd', '<cmd>Telescope git_files<cr>')
-set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
-set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
-set_keymap('n', '<leader>fl', '<cmd>Telescope lsp_document_symbols<cr>')
-set_keymap('n', '<leader>fk', '<cmd>Telescope keymaps<cr>')
-set_keymap('n', '<leader>fm', '<cmd>Telescope heading<cr>')
+set_keymap('n', '<c-p>', '<cmd>Telescope git_files<cr>')
+set_keymap('n', '<c-g>', '<cmd>Telescope live_grep<cr>')
+set_keymap('n', '<c-b>', '<cmd>Telescope buffers<cr>')
+set_keymap('n', '<c-l>', '<cmd>Telescope lsp_document_symbols<cr>')
 
 local actions = require('telescope.actions')
 require('telescope').setup({
